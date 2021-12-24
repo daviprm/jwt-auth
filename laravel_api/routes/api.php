416838c0da14142e4c0203b6e8e8e862d91f9aac
Davi Mello <daviprm@outlook.com>
2021-12-24 09:46:46 -0300

    feat: auth routes

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+Route::get('user', [App\Http\Controllers\AuthController::class, 'user'])->name('user');
